a486fb81feeef1e0d6d96fab8bbc8c55ecb4614e
maryaposohova <maryaposohova1202@gmail.com>
2024-10-12 17:27:48 +0500

    Add files via upload

--- module_6_hw_hard_figure.py
+++ module_6_hw_hard_figure.py
@@ -2,21 +2,24 @@ class Figure:
     sides_count = 0  # счет сторон
 
     def __init__(self, __sides: int, __color, filled: bool):
-        self.__sides = __sides  # (список сторон(целые числа))
-        self.__color = __color
+        self.__sides = []  # (список сторон(целые числа))
+        self.__color = []
         self.filled = filled  # (закрашенный, bool)
 
-    def get_color(self):
-        return []
+    def get_color(self, r, g, b):  # должен возвращать список RGB цветов
+        return self.__color.append()
 
-    def __is_valid_color(self, *values):
-        if not isinstance(values, int) or values < 0 or values > 255:
+    def __is_valid_color(self, r, g, b):
+        if (not isinstance(r, int) or r < 0 or r > 255 and not isinstance(g, int) or g < 0 or g > 255
+                and not isinstance(b, int) or b < 0 or b > 255):
             raise ValueError("Значение должно быть целым положительным числом в диапазоне от 0 до 255")
-        self.values = values
+        self.values = r
+        self.values = g
+        self.values = b
 
 
 d = Figure(3, 4,True)
-print(d.__dict__)
+print(d.get_color(-5, 7,8))
 #
 # circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
 # cube1 = Cube((222, 35, 130), 6)
