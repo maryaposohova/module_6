f617b61d8b9f60332a64dfcbdabe073df30b025a
maryaposohova <maryaposohova1202@gmail.com>
2024-10-12 11:42:11 +0500

    Add files via upload

--- ping-pong/game.py
+++ ping-pong/game.py
@@ -6,25 +6,38 @@ SCREEN_HEIGHT = 600
 SCREEN_TITLE = 'Pong game'
 
 # добавляем мячик
-class Ball(arcade.Sprite):
+class Ball(arcade.Sprite): # класс мячика
     def __init__(self):
         super().__init__('мячик.png', 0.1)
-        self.change_x = 0.5 # скорость перемещения
+        self.change_x = 3 # горизонтальная скорость перемещения
+        self.change_y = 2  # вертикальная скорость перемещения
     def update(self): # делаем мяч подвижным. метод отвечает за обновление текущего положения х и у
-        self.center_x += self.change_x  # изменение значения мячика
-        self.center_y += self.change_y
-
+        self.center_x += self.change_x  # изменение значения мячика по горизонтали
+        self.center_y += self.change_y  # изменение значения мячика по вертикали
+        if self.right > SCREEN_WIGTH:  # если мяч переезжает правую, если больше ширины окна
+            self.change_x = -self.change_x  # то мы меняем положение на противоположное
+        if self.left <= 0:  # если мяч переезжает правую, если больше ширины окна
+            self.change_x = -self.change_x  # то мы меняем положение на противоположное
+        if self.top > SCREEN_HEIGHT:  # если мяч улетает вверх га границу (больше чем высота окна))
+            self.change_y = -self.change_y  # то мы меняем положение на противоположное
+        if self.bottom <= 0:  #  если вниз за границу улетает, меньше или равно 0
+            self.change_y = -self.change_y  # то мы меняем положение на противоположное
 
 # добавляем ракетку
 class Bar(arcade.Sprite):  # класс ракетки наследуется от arcade.sprite
     def __init__(self):
-        super().__init__('ракетка.png', 0.2)
+        super().__init__('ракетка.png', 0.5)
         # если мы хотим, чтобы ракетка появилась в игре, то ее надо создать внутри класса Game,
         # а характеристики мы создаем в ините. В классе гейм нам тоженужно преопределить инит. Но передаем туда
         # ширину, высоту и заголовок, 15 и 16 строки
 
-
-
+    def update(self):
+        self.center_x += self.change_x
+        if self.right > SCREEN_WIGTH:  # если ракетка убегает вправо
+            self.right = SCREEN_WIGTH  # то приравниваем правую часть ракетки к ширине экрана
+            # то же самое делает с левой частью, приравниваем ее к экрану
+        if self.left <= 0:
+            self.left = 0
 class Game(arcade.Window):
     def __init__(self, wigth, height, title):
         super().__init__(wigth, height, title)  # и эти параметры мы передаем классу родителю
@@ -43,7 +56,21 @@ class Game(arcade.Window):
         self.ball.draw()
 
     def update(self, delta): # переопределяем метод апдейт, дельту не трогаем, это частота обновления
+        if arcade.check_for_collision(self.bar, self.ball):
+            self.ball.change_y = -self.ball.change_y
         self.ball.update()
+        self.bar.update()
+
+    def on_key_press(self, key, modifiers): # переопределяем метод, чтобы добавлять функционал, который будет реализовываться при нажатии и отпускании кнопки
+        if key == arcade.key.RIGHT: # достаем из библиотеки модуль кей.RIGHT - константа в модуле кей, соответствует кнопке на клавиатуре(стрелка )
+            self.bar.change_x = 20 # если положительное значение, то движется вправо
+        if key == arcade.key.LEFT:
+            self.bar.change_x = -20  # если отерицательное значение, то движется влево
+
+    def on_key_release(self, key, modifiers): # останавливаем ракетку при отпускании мышки
+        if key == arcade.key.RIGHT or key == arcade.key.LEFT:
+            self.bar.change_x = 0
+
 
 if __name__ == "__main__":
     window = Game(SCREEN_WIGTH, SCREEN_HEIGHT, SCREEN_TITLE)  # создание экземпляра класса гейм, передаем парамерты констант
